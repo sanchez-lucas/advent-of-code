59b1c1ef9148f47e957cd7c893b8e25fcddb92fa
Lucas Sanchez <lucas.sanchez.dev@gmail.com>
2025-12-08 10:49:45 +0100

    feat: use functools.cache for day07 part 2

--- 2025/day07/day07-part2.py
+++ 2025/day07/day07-part2.py
@@ -1,3 +1,5 @@
+import functools
+
 f = open("input.txt", "r")
 lines = [line.strip() for line in f.readlines()]
 
@@ -12,22 +14,18 @@ directions = {
   "LOWER-RIGHT": (1, 1),
 }
 
-def count_timelines(pos: tuple[int, int], pos_timelines_cache: dict) -> int:
-  if pos in pos_timelines_cache:
-    return pos_timelines_cache[pos]
+@functools.cache
+def count_timelines(pos: tuple[int, int]) -> int:
   if pos[0] < 0 or pos[0] >= len(lines) or pos[1] < 0 or pos[1] >= len(lines[0]):
-    pos_timelines_cache[pos] = 1
     return 1
   if lines[pos[0]][pos[1]] == "^":
-    left_timelines = count_timelines((pos[0] + directions["LEFT"][0], pos[1] + directions["LEFT"][1]), pos_timelines_cache)
-    right_timelines = count_timelines((pos[0] + directions["RIGHT"][0], pos[1] + directions["RIGHT"][1]), pos_timelines_cache)
+    left_timelines = count_timelines((pos[0] + directions["LEFT"][0], pos[1] + directions["LEFT"][1]))
+    right_timelines = count_timelines((pos[0] + directions["RIGHT"][0], pos[1] + directions["RIGHT"][1]))
     timelines = left_timelines + right_timelines
-    pos_timelines_cache[pos] = timelines
     return timelines
-  timelines = count_timelines((pos[0] + directions["LOWER"][0], pos[1] + directions["LOWER"][1]), pos_timelines_cache)
-  pos_timelines_cache[pos] = timelines
+  timelines = count_timelines((pos[0] + directions["LOWER"][0], pos[1] + directions["LOWER"][1]))
   return timelines
 
 S_pos = (0, lines[0].index("S"))
-res = count_timelines(S_pos, {})
+res = count_timelines(S_pos)
 print(res)
